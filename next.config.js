523e0257a62557e1d3edd417cf3c0b6da0ee72c2
Zephyr Mathias <wuttichai.junsoontraporn@gmail.com>
2020-04-21 20:04:39 +0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+  webpack: config => {
+    return config
+  },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
