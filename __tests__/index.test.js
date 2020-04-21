5fcb15b6dc73d2145f24deceebf445cd5ac4766b
Zephyr Mathias <wuttichai.junsoontraporn@gmail.com>
2020-04-21 20:22:57 +0700

    fix(test): move __tests__

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Index from './index'
+import Index from '../src/pages/index'
 
 test('snapshot', () => {
   const component = shallow(<Index />)
